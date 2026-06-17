bd1f3ed0006271791e6c0d0b71b9fbe4c28e64f7
Антон Скляров <sklarovanton833@gmail.com>
2026-06-17 21:32:19 +0500

    Task1

---
+++ Tyuiu.SklyarovAM.Sprint6.Task1.V2.Lib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.SklyarovAM.Sprint6.Task1.V2.Lib
+{
+    public class Class1
+    {
+    }
+}
---
+++ Tyuiu.SklyarovAM.Sprint6.Task1.V2.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SklyarovAM.Sprint6.Task1.V2.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SklyarovAM.Sprint6.Task1.V2.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4c1fb4e0-2024-4f6e-8fc1-8ece64ae991d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
